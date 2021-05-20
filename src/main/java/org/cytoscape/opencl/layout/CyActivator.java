bc04e5bfebc8b65b7df37fe73b4b3960618b777a
Mike Kucera <mikekucera@gmail.com>
2021-05-20 13:34:56 -0400

    Wait for opencl-cycl app to finish starting before initializing.

--- src/main/java/org/cytoscape/opencl/layout/CyActivator.java
+++ src/main/java/org/cytoscape/opencl/layout/CyActivator.java
@@ -1,43 +1,38 @@
 package org.cytoscape.opencl.layout;
 
-import static org.cytoscape.work.ServiceProperties.MENU_GRAVITY;
-import static org.cytoscape.work.ServiceProperties.PREFERRED_MENU;
-import static org.cytoscape.work.ServiceProperties.TITLE;
+import static org.cytoscape.work.ServiceProperties.*;
 
 import java.util.Properties;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-import org.cytoscape.application.CyApplicationConfiguration;
 import org.cytoscape.opencl.cycl.CyCL;
-import org.cytoscape.property.CyProperty;
 import org.cytoscape.service.util.AbstractCyActivator;
 import org.cytoscape.view.layout.CyLayoutAlgorithm;
-import org.cytoscape.work.ServiceProperties;
 import org.cytoscape.work.undo.UndoSupport;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class CyActivator extends AbstractCyActivator 
-{
-	public CyActivator() 
-	{
-		super();
+public class CyActivator extends AbstractCyActivator {
+
+	private final Logger logger = LoggerFactory.getLogger(CyActivator.class);
+	
+	public void start(BundleContext bc)  {	
+		// Have to wait for the opencl-cycl bundle to start and register its CyCL service.
+		registerServiceListener(bc, 
+			(cycl, props) -> initialize(bc, cycl),
+			(cycl, props) -> {}, 
+			CyCL.class);
 	}
-
-	public void start(BundleContext bc) 
-	{	
-		// Start OpenCL Layout in separate thread
-		final ExecutorService service = Executors.newSingleThreadExecutor();
-		service.submit(()-> {
+	
+	
+	private void initialize(BundleContext bc, CyCL cycl) {
+		new Thread(() -> {
 			try {
-				CyApplicationConfiguration applicationConfig = getService(bc, CyApplicationConfiguration.class);	
-				CyProperty<Properties> cyPropertyServiceRef = getService(bc, CyProperty.class, "(cyPropertyName=cytoscape3.props)");
-				
-				CyCL.initialize(applicationConfig, cyPropertyServiceRef);
-				
 				// Don't initialize if there are no OpenCL devices.
-				if (CyCL.getDevices().size() == 0)
+				if (CyCL.getDevices().size() == 0) {
+					logger.error("No OpenCL compatible device found. Cannot register '" + CLLayout.ALGORITHM_DISPLAY_NAME + "'.");
 					return;
+				}
 				
 				UndoSupport undo = getService(bc, UndoSupport.class);
 
@@ -52,7 +47,7 @@ public class CyActivator extends AbstractCyActivator
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		});
+		}).start();
 	}
 }
 
